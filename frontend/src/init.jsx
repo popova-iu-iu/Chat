7bafab1b74f42f98ab2933788aff223a5e03343e
popova iuliia <popova.iuliia.iu@gmail.com>
2023-04-29 18:23:38 +0300

    add rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -4,15 +4,23 @@ import leoProfanity from "leo-profanity";
 
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import { Provider as StoreProvider } from "react-redux";
+import { Provider as ProviderRollBar, ErrorBoundary } from "@rollbar/react";
 
 import App from "./App";
 
 import ru from "./locales/ru";
 import store from "./store";
 
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ROLLBAR,
+  environment: "production",
+};
+
+
+
 const init = async (socket) => {
   leoProfanity.add(leoProfanity.getDictionary("ru"));
-  
+
   const i18n = i18next.createInstance();
 
   await i18n.use(initReactI18next).init({
@@ -21,11 +29,15 @@ const init = async (socket) => {
   });
 
   const vdom = (
+     <ProviderRollBar config={rollbarConfig}>
+      <ErrorBoundary errorMessage="Error in React render">
     <StoreProvider store={store}>
       <I18nextProvider i18n={i18n}>
         <App socket={socket} />
       </I18nextProvider>
     </StoreProvider>
+    </ErrorBoundary>
+    </ProviderRollBar>
   );
 
   return vdom;
