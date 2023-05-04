ad7ddd2ca83d6c53f68adca24659e98b67b97d59
popova iuliia <popova.iuliia.iu@gmail.com>
2023-05-04 15:10:22 +0300

    fix locales

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import notFoundImg from '../../assets/notFoundImg.svg';
 
-const NotFound = () => (
-  <div className="text-center">
-    <img
-      alt="Страница не найдена"
-      className="img-fluid h-25"
-      src={notFoundImg}
-    />
-    <h1 className="h4 text-muted">Страница не найдена</h1>
-    <p className="text-muted">
-      Но вы можете перейти
-      {' '}
-      <a href="/">на главную страницу</a>
-    </p>
-  </div>
-);
+const NotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="text-center">
+      <img
+        alt={t('notFound.pageNotFound')}
+        className="img-fluid h-25"
+        src={notFoundImg}
+      />
+      <h1 className="h4 text-muted">{t('notFound.pageNotFound')}</h1>
+      <p className="text-muted">
+        {t('notFound.text')}
+        {' '}
+        <a href="/">{t('notFound.toMain')}</a>
+      </p>
+    </div>
+  );
+};
+
 export default NotFound;
